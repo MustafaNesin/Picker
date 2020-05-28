676b11e6ceebc2562ce6eb8889e65a5eb6bf67ff
Mustafa Nesin <mustafa.nesin@gazi.edu.tr>
2020-05-28 17:35:01 +0300

    Create 'ListFilterControl' and add into 'ListForm'

---
+++ Picker/Views/ListFilterControl.cs
@@ -0,0 +1,9 @@
+﻿namespace Picker.Views
+{
+    using System.Windows.Forms;
+
+    internal partial class ListFilterControl : UserControl
+    {
+        public ListFilterControl() => InitializeComponent();
+    }
+}
