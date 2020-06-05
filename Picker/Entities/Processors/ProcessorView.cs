0abbb08ba3867e2ca9db83051709c1c8254675f4
Cem Ufuk YILMAZ <cemufuk.yilmaz@gazi.edu.tr>
2020-06-06 00:02:59 +0300

    Fix EntityName in ProcessorView.

--- Picker/Entities/Processors/ProcessorView.cs
+++ Picker/Entities/Processors/ProcessorView.cs
@@ -38,7 +38,7 @@
             get => string.IsNullOrWhiteSpace(ProcessorModel)
                 ? ProcessorModel
                 : $"{ProcessorBrand.Name} {ProcessorFamily}-{ProcessorModel} " +
-                  $"{ProcessorFrequency} GHz {ProcessorCores} Çekirdek {ProcessorCacheSize} MB Önbellek";
+                  $"{ProcessorFrequency/1000M} GHz {ProcessorCores} Çekirdek {ProcessorCacheSize} MB Önbellek";
             set => Text = value;
         }
 
